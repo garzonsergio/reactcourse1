0f640eab89e83699cd902c0b499c048224fbe6b6
Sergio C Garzon <scgarzonp@gmail.com>
2022-03-25 09:15:22 -0500

    Unnecessary content deleted

--- src/App.js
+++ src/App.js
@@ -1,26 +1,8 @@
-import logo from "./logo.svg";
-import "./App.css";
 
-function App(props) {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React in the middle of the Night <br />
-          {props.titulo}
-        </a>
-      </header>
-    </div>
-  );
+// import "./App.css";
+
+function App() {
+  return ( );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+// import "./index.css";
 import App from "./App";
 
-ReactDOM.render(
-  <App titulo="Señor Don Titulo" />,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
