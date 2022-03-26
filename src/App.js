ecc183f26e333c803c65ed98a7ab94cba6c050e6
Sergio C Garzon <scgarzonp@gmail.com>
2022-03-26 14:49:36 -0500

    creation of components and some styles

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
-
+import React from "react";
+import { TodoCounter } from "./TodoCounter";
+import { TodoSearch } from "./TodoSearch";
+import { TodoList } from "./TodoList";
+import { TodoItem } from "./TodoItem";
 // import "./App.css";
 
+const todos = [
+  { text: "Limpiar baños", completed: false },
+  { text: "Pararse en las manos", completed: false },
+  { text: "Sacar la basura", completed: false },
+];
+
 function App() {
-  return ( );
+  return (
+    <React.Fragment>
+      <TodoCounter />
+
+      <TodoSearch />
+
+      <TodoList>
+        {todos.map((todo) => (
+          <TodoItem key={todo.text} text={todo.text} />
+        ))}
+      </TodoList>
+      {/* <CreateTodoButton /> */}
+      <button>+</button>
+    </React.Fragment>
+  );
 }
 
 export default App;
