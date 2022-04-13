2eb28c961bbc838a987cc113b7b765ca84148a88
Sergio C Garzon <scgarzonp@gmail.com>
2022-04-13 10:13:12 -0500

    The context was used in TodoCounter component

--- src/TodoCounter/index.js
+++ src/TodoCounter/index.js
@@ -1,11 +1,17 @@
 import React from "react";
 import "./TodoCounter.css";
 
+import { TodoContext } from "../TodoContext";
+
 function TodoCounter({ total, completed }) {
   return (
-    <h2 className="TodoCounter">
-      Has completado {completed} de {total} tareas
-    </h2>
+    <TodoContext.Consumer>
+      {({ completedTodos, totalTodos }) => (
+        <h2 className="TodoCounter">
+          Has completado {completedTodos} de {totalTodos} tareas
+        </h2>
+      )}
+    </TodoContext.Consumer>
   );
 }
 
